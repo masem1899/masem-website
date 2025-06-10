576116db43b1c286023a4df093369f7e17c98ddb
Mario SEMPER <mario.semper@uniqa.at>
2025-06-10 16:15:29 +0200

    feat: implement subscriber handling and welcome email functionality

--- masem/frontend/api/save-subscriber.js
+++ masem/frontend/api/save-subscriber.js
@@ -1,3 +1,5 @@
+import fs from 'fs';
+import path from 'path';
 import { getApps, initializeApp, cert } from 'firebase-admin/app';
 import { getFirestore} from 'firebase-admin/firestore';
 import { Resend } from 'resend';
@@ -19,5 +21,53 @@ if(!getApps().length) {
 const db = getFirestore();
 
 export default async function handler(req, res) {
-    
+    if (req.method !== 'POST') return res.status(405).json({ error:'Method not allowed.' });
+
+    const { email } = req.body;
+
+    if (!email ||
+        !email.toLowerCase()
+        .match(/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/)) {
+            return res.status(400).json({ error:'Invalid email address.' });
+    }
+
+    try {
+        const snapshot = await db.collection('subscribers')
+            .where('email', '==', email)
+            .limit(1)
+            .get();
+
+        if (!snapshot.empty) {
+            return res.status(200).json({ message: 'Already subscribed.'} );
+        }
+
+        // Store subscriber
+        await db.collection('subscribers').add({
+            email,
+            createdAt: new Date(),
+            status: 'subscribed'
+        });
+
+        // Load html from file
+        const html = fs.readFileSync(path.resolve('./email-templates/welcome.html'), 'utf-8');
+
+        // Add to audience
+        await resend.contacts.create({
+            email,
+            audienceId: 'b398f236-bb01-4d41-8ace-e9159e7eea80',
+        });
+
+        // Send welcome mail
+        await resend.emails.send({
+            from: 'masem <subscription@masem.at>',
+            to: email,
+            subject: 'Welcome to masem!',
+            html: html,
+        });
+
+        return res.status(200).json({ success: true });
+    } catch(err) {
+        console.error('API error: ', err);
+        return res.status(500).json({ error: err.message });
+    }
 }
