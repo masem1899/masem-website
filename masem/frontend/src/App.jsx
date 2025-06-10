2931a8ebd3257c404f1e6f9a2c564f1c62488db6
Mario SEMPER <mario.semper@uniqa.at>
2025-06-10 08:14:55 +0200

    fix: correct import statement for SpeedInsights in App component

--- masem/frontend/src/App.jsx
+++ masem/frontend/src/App.jsx
@@ -6,8 +6,10 @@ import AboutMe from "./components/landing/AboutMe"
 import Footer from "./components/Footer"
 import ToggleDarkMode from "./components/ToggleDarkMode"
 import { Analytics } from '@vercel/analytics/react';
-import { SpeedInsights } from '@vercel/speed-insights';
 import { SpeedInsights } from "@vercel/speed-insights/react"
+
+
+
 
 function App() {
   return (
