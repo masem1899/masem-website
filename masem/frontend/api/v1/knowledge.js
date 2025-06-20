f35a7b9f1c0dc59cb809687cd5a0ff9796a54cb8
Mario SEMPER <mario.semper@uniqa.at>
2025-06-20 13:59:11 +0200

    Implement structural updates and optimizations across multiple modules

--- masem/frontend/api/v1/knowledge.js
+++ masem/frontend/api/v1/knowledge.js
@@ -4,10 +4,10 @@ import { pool, pgPlaceholders } from './_neon.js';
 
 
 
-const table = 'posts';
+const table = 'knowledge';
 
-// GET posts
-export default async function posts(req, res) {
+// GET knowledge
+export default async function knowledge(req, res) {
     const { sql, params } = buildSQL(req, { table: table });
     const { rows } = await pool.query(pgPlaceholders(sql), params);
     return res.status(200).json(rows);
--- masem/frontend/server.mjs
+++ masem/frontend/server.mjs
@@ -2,9 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import projects from './api/v1/projects.js';
 import posts from './api/v1/posts.js';
-//import getProjectsHandler from './api/v1/projects.js';
-//import postsRouter from './api/v1/posts.js';
-//import  { saveSubscription } from './api/v1/subscriptions.js';
+import knowledge from './api/v1/knowledge.js';
 
 const API_BASE = '/api/v1';
 
@@ -14,23 +12,9 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// projects
-//app.get(`${API_BASE}/projects`, getProjectsHandler);
-//app.get(`${API_BASE}/project/:slug`, getProjectsHandler);
 app.get(`${API_BASE}/projects`, projects);
 app.get(`${API_BASE}/posts`, posts);
-// posts
-// app.get(`${API_BASE}/posts`, postsRouter);
-// app.get(`${API_BASE}/posts/:slug`, postsRouter);
-// app.post(`${API_BASE}/posts`, postsRouter);
-
-// subscriptions
-// app.post(`${API_BASE}/subscriptions`, saveSubscription);
-
-// subscriptions
-// app.post('/api/save-subscriber', async (req, res) => {
-//     return getProjectsHandler(req, res);
-
+app.get(`${API_BASE}/knowledge`, knowledge);
 
 app.listen(8787, () => {
     console.info('API server running on localhost:8787');
