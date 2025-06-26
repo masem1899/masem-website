30908277b5836041201106c46e637ff2b5ed96f8
Mario SEMPER <mario.semper@uniqa.at>
2025-06-26 12:33:30 +0200

    feat: implement scheduled Bing IndexNow API integration

---
+++ masem/frontend/api/sd_bing.ts
@@ -0,0 +1,44 @@
+export const config = {
+    schedule: '0 * * * *', // Every hour
+}
+
+
+export default async function(req, res) {
+    console.log("onSchedule", req);
+
+    try {
+        const bingRes = await fetch('https://api.indexnow.org/IndexNow', {
+            method: "POST",
+            headers: { 'Content-Type':'application/json' },
+            body: JSON.stringify({
+                host:"https://masem.at",
+                key:'ea8b3151b6094ffe88a2012bce6e9b28',
+                urlList: [
+                    'https://masem.at',
+                    'https://masem.at/projects/pyseoa',
+                    'https://masem.at/projects/pyseoa-ts',
+                ]
+            }),
+        });
+    } catch(err) {
+        console.error('error calling bing newIndex:', err);
+    }
+
+    return res.status(200).json({ ok:true });
+}
+
+/*
+POST /IndexNow HTTP/1.1
+Content-Type: application/json; charset=utf-8
+Host: api.indexnow.org
+{
+  "host": "www.example.org",
+  "key": "ea8b3151b6094ffe88a2012bce6e9b28",
+  "keyLocation": "https://www.example.org/ea8b3151b6094ffe88a2012bce6e9b28.txt",
+  "urlList": [
+      "https://www.example.org/url1",
+      "https://www.example.org/folder/url2",
+      "https://www.example.org/url3"
+      ]
+}
+*/
