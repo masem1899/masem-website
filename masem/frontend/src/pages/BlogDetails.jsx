5a3bbf91b61a56fca9aa73ddb1c4b1323409afea
Mario SEMPER <mario.semper@uniqa.at>
2025-06-26 07:01:03 +0200

    feat: add SeoMeta import to BlogDetails component

--- masem/frontend/src/pages/BlogDetails.jsx
+++ masem/frontend/src/pages/BlogDetails.jsx
@@ -4,6 +4,7 @@ import { useParams } from "react-router-dom"
 import ReactMarkdown from 'react-markdown';
 import remarkGfm from "remark-gfm";
 import rehypeRaw from "rehype-raw";
+import { SeoMeta } from "@/components/own/SeoMeta";
 
 
 
