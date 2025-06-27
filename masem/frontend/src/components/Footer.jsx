d2fce1689eb4c6074e3a7d4c7250556b03d4a31c
Mario SEMPER <contact@masem.at>
2025-06-28 01:20:09 +0200

    Refactor code structure for improved readability and maintainability

--- masem/frontend/src/components/Footer.jsx
+++ masem/frontend/src/components/Footer.jsx
@@ -8,6 +8,9 @@ export default function Footer() {
         <footer className="text-muted-foreground bg-background">
         <div className="py-10 text-center text-sm flex flex-col items-center space-y-2">
             <div>
+                <script type="text/javascript" src="https://cdnjs.buymeacoffee.com/1.0.0/button.prod.min.js" data-name="bmc-button" data-slug="masem" data-color="#FFDD00" data-emoji=""  data-font="Cookie" data-text="Buy me a coffee" data-outline-color="#000000" data-font-color="#000000" data-coffee-color="#ffffff" ></script>
+            </div>
+            <div>
                 <a href="#legal" className="mx-2 hover:underline">Legal Notice</a>
                 <a href="#privacy" className="mx-2 hover:underline">Privacy Policy</a>
             </div>
