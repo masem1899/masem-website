6894931e200acfdd5cad14dc7b8c15d2a21f00fc
Mario SEMPER <contact@masem.at>
2025-06-28 01:25:04 +0200

    fix: add Buy Me A Coffee button to footer

--- masem/frontend/src/components/Footer.jsx
+++ masem/frontend/src/components/Footer.jsx
@@ -8,6 +8,7 @@ export default function Footer() {
         <footer className="text-muted-foreground bg-background">
         <div className="py-10 text-center text-sm flex flex-col items-center space-y-2">
             <div>
+                <a href="https://www.buymeacoffee.com/masem" target="_blank"><img src="https://cdn.buymeacoffee.com/buttons/v2/default-yellow.png" alt="Buy Me A Coffee" style={{height: '60px !important', width: '217px !important'}} /></a>
                 <script type="text/javascript" src="https://cdnjs.buymeacoffee.com/1.0.0/button.prod.min.js" data-name="bmc-button" data-slug="masem" data-color="#FFDD00" data-emoji=""  data-font="Cookie" data-text="Buy me a coffee" data-outline-color="#000000" data-font-color="#000000" data-coffee-color="#ffffff" ></script>
             </div>
             <div>
