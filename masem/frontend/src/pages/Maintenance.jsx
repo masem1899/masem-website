f5e45e9f1c6460a974eeb14de5d784f004214318
Mario Semper <contact@masem.at>
2025-09-11 15:40:12 +0200

    Update Maintenance.jsx

--- masem/frontend/src/pages/Maintenance.jsx
+++ masem/frontend/src/pages/Maintenance.jsx
@@ -5,7 +5,7 @@ function Maintenance() {
         <div className="min-h-screen flex flex-col items-center justify-center text-center">
             <div className="mb-8 flex items-end justify-center">
                 <img src="masemIT-logo-512x512.png" alt="MASEM Logo" className="h-64" />
-                <a herf="https://ai-ttract.com/?utm_source=site&utm_medium=maintenance&utm_campaign=masemIT" target="_blank>
+                <a herf="https://ai-ttract.com/?utm_source=site&utm_medium=maintenance&utm_campaign=masemIT" target="_blank">
                     <img src="ai-ttract-logo-nav-transparent.png" alt="ai-ttract Logo" className="h-64" />
                 </a>
             </div>
